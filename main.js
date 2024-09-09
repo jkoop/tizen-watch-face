c8ed9838d6a5a2ac56818011e262e32043caa0d2
Joe Koop <joe@joekoop.com>
2024-09-09 08:45:34 -0500

    Mon 09 Sep 2024 08:45:34 AM CDT

--- main.js
+++ main.js
@@ -4,7 +4,8 @@
 	var minuteAngle = 0;
 	var secondAngle = 0;
 	var stepCount = -1;
-	
+	var batt = '?';
+
     /**
      * @param {string} elementId - ID of the element to be rotated
      * @param {number} angle - angle of rotation, in turns
@@ -13,14 +14,15 @@
         const element = document.getElementById(elementId);
         element.style.transform = "rotate(" + angle + "turn)";
     }
-    
+
     // set the text of stuff and rotate things
     function draw() {
     	document.getElementById("dom").innerText = dom;
     	rotateElement("hand-hour", hourAngle);
     	rotateElement("hand-minute", minuteAngle);
     	rotateElement("hand-second", secondAngle);
-    	document.getElementById("step-count").innerText = stepCount + ' steps';
+    	// document.getElementById("step-count").innerText = stepCount + ' steps';
+    	document.getElementById("batt").innerHTML = batt;
     }
 
     /**
@@ -42,17 +44,24 @@
     }
 
     function updateStepCount(pedometerInfo) {
-    	stepCount = pedometerInfo.cumulativeTotalStepCount;
+    	stepCount = pedometerInfo.accumulativeTotalStepCount;
     	draw();
     }
-    
+
+    function updateBatt(batteryInfo) {
+//    	if (batteryInfo.timeToDischarge > 0.05) {
+//    		batt = batteryInfo.timeToDischarge + 'm';
+//    	} else if (batteryInfo.timeToFullCharge > 0.05) {
+//    		batt = batteryInfo.timeToFullCharge + 'm';
+//    	} else {
+    		batt = Math.round(batteryInfo.level * 100) + '%';
+//    	}
+    }
+
     /**
      * Initiates the application
      */
     function init() {
-        // step counter
-        tizen.humanactivitymonitor.setAccumulativePedometerListener(updateStepCount);
-
         // update the screen immediately when the device wakes up
         document.addEventListener("visibilitychange", function() {
             if (!document.hidden) {
@@ -60,10 +69,20 @@
             }
         });
 
+    	try {
+    	// battery level
+    	tizen.systeminfo.getPropertyValue("BATTERY", updateBatt, null);
+    	tizen.systeminfo.addPropertyValueChangeListener("BATTERY", updateBatt);
+
+        // step counter
+//        tizen.humanactivitymonitor.setAccumulativePedometerListener(updateStepCount);
+
+
         // update the screen when the time zone is changed
         tizen.time.setTimezoneChangeListener(function() {
             updateTime();
         });
+    	}catch(err){}
         
         // Update the watch hands every second
         setInterval(function() {
